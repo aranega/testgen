d79dbb471e700e3b22c56ea7c7c6dd86e7f47f95
vince <vince@saraswati>
2012-11-15 09:34:29 +0100

    commit by genmymodel: v20121115_093429

---
+++ test/test-component-parent/test-simpleclass-component/src/main/java/test/simpleclass/component/controller/SimpleClassController.java
@@ -0,0 +1,69 @@
+package test.simpleclass.component.controller;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import test.wrapper.SimpleClassWrapper;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import com.google.inject.Singleton;
+import javafx.fxml.FXML;
+
+//Start of user code for imports
+// TODO: import me!
+//End of user code
+
+//
+
+
+@Singleton
+public class SimpleClassController implements Initializable
+{
+	//Start of user code for attributes
+	// Other attributes
+	//End of user code
+	@FXML
+	private TextField simpleClassId;
+	
+	private SimpleClassWrapper simpleClassWrapper;
+    
+
+	@FXML
+	public void handleCancelButtonAction (ActionEvent event) 
+	{
+		//Start of user code handleCancelButtonAction
+		// TOTO: handleCancelButtonAction
+		// End of user code	
+	}
+
+	@FXML
+	public void handleSubmitButtonAction (ActionEvent event) 
+	{
+		//Start of user code handleSubmitButtonAction
+		// TOTO: handleSubmitButtonAction
+		// End of user code	
+	}
+
+	
+	public void initialize (URL args0, ResourceBundle args1) 
+	{
+		// Bindings
+		simpleClassId.setText(String.valueOf(simpleClassWrapper.getId()));
+		
+		simpleClassId.textProperty().bindBidirectional(simpleClassWrapper.idProperty(), new javafx.util.converter.NumberStringConverter());
+		
+			
+	}
+
+	
+	public void setSimpleClassWrapper (SimpleClassWrapper w) 
+	{
+		this.simpleClassWrapper = w;	
+	}
+
+
+	//Start of user code for methods
+	// Other methods
+	//End of user code
+
+}
