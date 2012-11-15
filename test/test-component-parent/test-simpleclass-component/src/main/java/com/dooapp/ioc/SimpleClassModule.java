3e66434af1f4436032385ce337cf48d61d3ef02a
vince <vince@saraswati>
2012-11-15 17:14:16 +0100

    commit by genmymodel: v20121115_171416

---
+++ test/test-component-parent/test-simpleclass-component/src/main/java/com/dooapp/ioc/SimpleClassModule.java
@@ -0,0 +1,44 @@
+package com.dooapp.ioc;
+
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dooapp.annotation.SimpleClass;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+/**
+ * This is the main Guice module of SimpleClass<br>
+ * As far as it is in the <code>com.dooapp.ioc</code> package, 
+ * it will be automatically registered during the application startup.
+ * <br>
+ * Created at : 1352996042<br>
+ * 
+ * @author genmymodel
+ * @since 1.0-SNAPSHOT
+ * 
+ */
+public class SimpleClassModule extends AbstractModule {
+	/**
+	 * The logger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(SimpleClassModule.class);
+
+	/**
+	 * {@inheritDoc}
+	 */
+
+	@Override
+	protected void configure() {
+	}
+
+	@Singleton
+	@Provides
+	@SimpleClass
+	private ResourceBundle provideResourceBundle() {
+		return ResourceBundle.getBundle("SimpleClass");
+	}
+}
---
+++ test/test-core-parent/test-entity/src/main/java/com/dooapp/test/entity/SimpleClass.java
@@ -0,0 +1,63 @@
+package com.dooapp.test.entity;
+
+import java.util.UUID;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+//Start of user code for imports
+// TODO: import me!
+//End of user code
+
+//
+
+
+@Entity
+public class SimpleClass
+{
+	//Start of user code for attributes
+	// Other attributes
+	//End of user code
+	@Id
+	@GeneratedValue(strategy = javax.persistence.GenerationType.AUTO)
+	private Long id;
+	
+	private UUID uuid;
+    
+
+	
+	public SimpleClass () 
+	{
+		uuid = UUID.randomUUID();	
+	}
+
+	
+	public Long getId () 
+	{
+		return this.id;	
+	}
+
+	
+	public UUID getUuid () 
+	{
+		return uuid;	
+	}
+
+	
+	public void setId (Long myId) 
+	{
+		this.id = myId;	
+	}
+
+	
+	public void unsetId () 
+	{
+		this.id = null;	
+	}
+
+
+	//Start of user code for methods
+	// Other methods
+	//End of user code
+
+}
