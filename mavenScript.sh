6fb73920c30d9df013663fa754958d5615d5617d
Vincent Aranega <vince@saraswati.(none)>
2012-11-16 14:23:30 +0100

    clean

--- mavenScript.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mkdir -p test/test-component-parent/ && cd test/test-component-parent/ && mvn archetype:generate -B -DarchetypeGroupId=com.dooapp -DarchetypeArtifactId=component-archetype -DgroupId=com.dooapp -DartifactId=test-simpleclass-component -Dversion=1.0-SNAPSHOT -DFileShortName=SimpleClass -DJavaShortName=SimpleClass -Dauthor=genmymodel -Dtime=`date +%s`
