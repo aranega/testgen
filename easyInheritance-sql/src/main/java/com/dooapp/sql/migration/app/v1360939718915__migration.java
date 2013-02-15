b032e37d44f07d3687d17f47f52f695c132241f3
genmymodel <help@genmymodel.com>
2013-02-15 15:48:43 +0100

    commit by genmymodel from www.genmymodel.com: v20130215_154841

---
+++ easyInheritance-sql/src/main/java/com/dooapp/sql/migration/app/v1360939718915__migration.java
@@ -0,0 +1,42 @@
+/*********************************************
+	@
+	@ WARNING: DO NOT DELETE THIS FILE!
+	@
+	version GenMyModel
+	http://www.genmymodel.com
+
+	Generator version: 28 December 2012
+
+	Operations :
+		
+		
+**********************************************/
+
+package com.dooapp.sql.migration.app;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.googlecode.flyway.core.migration.java.JavaMigration;
+
+/**
+ * 
+ * @author Genmymodel
+ *
+ */
+public class v1360939718915__migration implements JavaMigration {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void migrate(JdbcTemplate jdbcTemplate) throws Exception {	
+		
+		
+
+
+
+
+	}
+}
+
+
