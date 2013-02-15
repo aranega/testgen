1d0816e0c851df0c20ec3e7f9495c6149dd2c9df
GenMyModel <github@genmymodel.com>
2013-02-15 17:08:07 +0100

    extends object

--- easyInheritance-core-parent/easyInheritance-entity/src/main/java/com/dooapp/easyInheritance/entity/DBean.java
+++ easyInheritance-core-parent/easyInheritance-entity/src/main/java/com/dooapp/easyInheritance/entity/DBean.java
@@ -17,7 +17,7 @@ import com.dooapp.lib.common.entity.Bean;
 
 public class DBean
 //Start of user code bloc for inheritance
-
+extends Object
 //End of user code
 //Start of user code bloc for interfaces
  implements Bean
