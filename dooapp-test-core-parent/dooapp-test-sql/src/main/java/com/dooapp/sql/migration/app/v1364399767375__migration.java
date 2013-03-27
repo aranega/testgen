07102f6497d161cbdca49686dc079a4bd22eb196
genmymodel <help@genmymodel.com>
2013-03-27 16:56:17 +0100

    commit by genmymodel from www.genmymodel.com: v20130327_165611

---
+++ dooapp-test-core-parent/dooapp-test-sql/src/main/java/com/dooapp/sql/migration/app/v1364399767375__migration.java
@@ -0,0 +1,48 @@
+/*********************************************
+	@
+	@ WARNING: DO NOT DELETE THIS FILE!
+	@
+	version GenMyModel
+	http://www.genmymodel.com
+
+	Generator version: 28 December 2012
+
+	Operations :
+		
+		
+		
+		
+		
+**********************************************/
+
+package com.dooapp.sql.migration.app;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.googlecode.flyway.core.migration.java.JavaMigration;
+
+/**
+ * 
+ * @author Genmymodel
+ *
+ */
+public class v1364399767375__migration implements JavaMigration {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void migrate(JdbcTemplate jdbcTemplate) throws Exception {	
+		
+		
+		
+		
+		
+
+
+
+
+	}
+}
+
+
