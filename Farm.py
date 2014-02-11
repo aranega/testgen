539ca32970bdf77f46e4abbc4dd8dc9b3ac4bcba
genmymodel <help@genmymodel.com>
2014-02-11 17:09:53 +0100

    commit by genmymodel from www.genmymodel.com: v20140211_170951

--- Farm.py
+++ Farm.py
@@ -2,54 +2,7 @@
  Just a little fun model for a kinda farm managment.
 """
 
-class MyClass(object):
-	pass
-	# Start of user code -> properties/constructors for MyClass class
 
-	# End of user code
-	# Start of user code -> methods for MyClass class
-
-	# End of user code
-
-
-class Barn(object):
-	def __init__(self):
-		self.farm = None
-		self.id = ""
-		
-	# Start of user code -> properties/constructors for Barn class
-
-	# End of user code
-	# Start of user code -> methods for Barn class
-
-	# End of user code
-
-
-
-
-class Cows(object):
-	def __init__(self):
-		self.name = ""
-		self.eatAt = None
-		self.sleepIn = None
-		
-	# Start of user code -> properties/constructors for Cows class
-
-	# End of user code
-	# Start of user code -> methods for Cows class
-
-	# End of user code
-
-class Field(object):
-	def __init__(self):
-		self.farm = None
-		
-	# Start of user code -> properties/constructors for Field class
-
-	# End of user code
-	# Start of user code -> methods for Field class
-
-	# End of user code
 
 
 class Farm(object):
@@ -66,6 +19,53 @@ class Farm(object):
 
 	# End of user code
 
+
+class MyClass(object):
+	pass
+	# Start of user code -> properties/constructors for MyClass class
+
+	# End of user code
+	# Start of user code -> methods for MyClass class
+
+	# End of user code
+
+class Barn(object):
+	def __init__(self):
+		self.farm = None
+		self.id = ""
+		
+	# Start of user code -> properties/constructors for Barn class
+
+	# End of user code
+	# Start of user code -> methods for Barn class
+
+	# End of user code
+
+
+class Cows(object):
+	def __init__(self):
+		self.eatAt = None
+		self.name = ""
+		self.sleepIn = None
+		
+	# Start of user code -> properties/constructors for Cows class
+
+	# End of user code
+	# Start of user code -> methods for Cows class
+
+	# End of user code
+
+
+class Field(object):
+	def __init__(self):
+		self.farm = None
+		
+	# Start of user code -> properties/constructors for Field class
+
+	# End of user code
+	# Start of user code -> methods for Field class
+
+	# End of user code
 
 
 # Start of user code -> functions/methods for Farm package
