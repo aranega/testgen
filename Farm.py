259b518b5e016817d9e843134a2e3768ee727f1e
genmymodel <help@genmymodel.com>
2014-02-11 17:07:44 +0100

    commit by genmymodel from www.genmymodel.com: v20140211_170743

---
+++ Farm.py
@@ -0,0 +1,73 @@
+"""
+ Just a little fun model for a kinda farm managment.
+"""
+
+class MyClass(object):
+	pass
+	# Start of user code -> properties/constructors for MyClass class
+
+	# End of user code
+	# Start of user code -> methods for MyClass class
+
+	# End of user code
+
+
+class Barn(object):
+	def __init__(self):
+		self.farm = None
+		self.id = ""
+		
+	# Start of user code -> properties/constructors for Barn class
+
+	# End of user code
+	# Start of user code -> methods for Barn class
+
+	# End of user code
+
+
+
+
+class Cows(object):
+	def __init__(self):
+		self.name = ""
+		self.eatAt = None
+		self.sleepIn = None
+		
+	# Start of user code -> properties/constructors for Cows class
+
+	# End of user code
+	# Start of user code -> methods for Cows class
+
+	# End of user code
+
+class Field(object):
+	def __init__(self):
+		self.farm = None
+		
+	# Start of user code -> properties/constructors for Field class
+
+	# End of user code
+	# Start of user code -> methods for Field class
+
+	# End of user code
+
+
+class Farm(object):
+	def __init__(self):
+		self.cows = []
+		self.name = ""
+		self.ownedFields = None
+		self.ownedBarns = []
+		
+	# Start of user code -> properties/constructors for Farm class
+
+	# End of user code
+	# Start of user code -> methods for Farm class
+
+	# End of user code
+
+
+
+# Start of user code -> functions/methods for Farm package
+
+# End of user code
