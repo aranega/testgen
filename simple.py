8e882a3dbdd547e25b130798aa201542c0e8f5be
vince <vincent.aranega@gmail.com>
2014-02-14 10:28:44 +0100

    commit by vince using genmymodel: v20140214_102842

---
+++ simple.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+class A(object):
+	"""
+	 Doc
+	"""
+	def __init__(self):
+		self.name = None
+		
+	# Start of user code -> properties/constructors for A class
+
+	# End of user code
+	def getName(self):
+		# Start of user code protected zone for getName function body
+		return None
+		# End of user code	
+	# Start of user code -> methods for A class
+
+	# End of user code
+
+class MyInterface(object):
+	pass
+	# Start of user code -> properties/constructors for MyInterface class(interface)
+
+	# End of user code
+	# Start of user code -> methods for MyInterface class(interface)
+
+	# End of user code
+
+
+class B(A, C):
+	"""
+	 Doc
+	"""
+	def __init__(self):
+		self.attribute = datetime()
+		self.c = None
+		
+	# Start of user code -> properties/constructors for B class
+
+	# End of user code
+	# Start of user code -> methods for B class
+
+	# End of user code
+
+class C(MyInterface):
+	def __init__(self):
+		self.b = None
+		
+	# Start of user code -> properties/constructors for C class(interface)
+
+	# End of user code
+	# Start of user code -> methods for C class(interface)
+
+	# End of user code
+
+
+# Start of user code -> functions/methods for simple package
+
+# End of user code
