c4056ba71629076c24461a7ff73063ace27ab8cb
GenMyModel <help@genmymodel.com>
2014-04-25 15:07:41 +0200

    commit by GenMyModel using genmymodel: v20140425_150740

---
+++ src/main/java/simple/mypackage/A.java
@@ -0,0 +1,54 @@
+package simple.mypackage;
+import simple.MyInterface;
+
+
+/**
+ * This element represents a A type of objects
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class A
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String name;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public z myClass;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public A(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public MyInterface operation() {
+		// TODO : to implement
+		return null;	
+	}
+	
+}
+
---
+++ src/main/java/simple/mypackage/z.java
@@ -0,0 +1,42 @@
+package simple.mypackage;
+import simple.java.util.List;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class z
+{
+	/**
+	 * @GeneratedValue
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public java.util.List id;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public A a;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public z(){
+		super();
+	}
+
+}
+
