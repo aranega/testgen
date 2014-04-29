ecf58fb8ad3b908d515c2024e9c9ddee31776d89
Vincent Aranega <vincent.aranega@gmail.com>
2014-04-29 17:00:27 +0200

    commit by Vincent Aranega using genmymodel: v20140429_170026

--- simple.py
+++ simple.py
@@ -1,18 +1,18 @@
-class MyClass3(object):
-	pass
-	# Start of user code -> properties/constructors for MyClass3 class
-
-	# End of user code
-	# Start of user code -> methods for MyClass3 class
-
-	# End of user code
-
 class MyClass(object):
 	pass
 	# Start of user code -> properties/constructors for MyClass class
 
 	# End of user code
 	# Start of user code -> methods for MyClass class
+
+	# End of user code
+
+class MyClass3(object):
+	pass
+	# Start of user code -> properties/constructors for MyClass3 class
+
+	# End of user code
+	# Start of user code -> methods for MyClass3 class
 
 	# End of user code
 
