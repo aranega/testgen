b8fabd77eb1d7b276582cdcdfaace7377b411af9
aranega <help@genmymodel.com>
2017-08-30 14:27:25 +0000

    commit by aranega using genmymodel: v20170830_142723

---
+++ src/model/MyClass.java
@@ -0,0 +1,49 @@
+package model;
+
+
+/**
+* @generated
+*/
+public class MyClass {
+    
+    /**
+    * @generated
+    */
+    private String attribute;
+    
+    /**
+    * @generated
+    */
+    private String attribute2;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public String getAttribute() {
+        return this.attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getAttribute2() {
+        return this.attribute2;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAttribute2(String attribute2) {
+        this.attribute2 = attribute2;
+    }
+    
+}
