4e66df1db5c44203f73cc4f56212aef4ffa0f268
aranega <help@genmymodel.com>
2018-02-01 10:54:27 +0000

    commit by aranega using genmymodel: v20180201_105424

---
+++ src/model/MyClass2.java
@@ -0,0 +1,49 @@
+package model;
+
+
+/**
+* @generated
+*/
+public class MyClass2 {
+    
+    /**
+    * @generated
+    */
+    private Boolean attribute;
+    
+    /**
+    * @generated
+    */
+    private String attribute2;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Boolean getAttribute() {
+        return this.attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public Boolean setAttribute(Boolean attribute) {
+        this.attribute = attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getAttribute2() {
+        return this.attribute2;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAttribute2(String attribute2) {
+        this.attribute2 = attribute2;
+    }
+    
+}
