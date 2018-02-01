68935b0f8ea644bcc41b5e1248bdb729b0e344f3
aranega <help@genmymodel.com>
2018-02-01 10:56:00 +0000

    commit by aranega using genmymodel: v20180201_105557

--- src/model/MyClass2.java
+++ src/model/MyClass2.java
@@ -9,7 +9,7 @@ public class MyClass2 {
     /**
     * @generated
     */
-    private boolean attribute;
+    private Boolean attribute;
     
     /**
     * @generated NOT
@@ -21,18 +21,18 @@ public class MyClass2 {
     /**
     * @generated
     */
-    public boolean getAttribute() {
+    public Boolean getAttribute() {
         return this.attribute;
     }
     
     /**
     * @generated
     */
-    public boolean setAttribute(boolean attribute) {
+    public Boolean setAttribute(Boolean attribute) {
         this.attribute = attribute;
     }
-    
-    /**
+
+				/**
     * @generated NOT
     */
     public int getAttribute2() {
@@ -40,6 +40,13 @@ public class MyClass2 {
     }
     
     /**
+    * @generated
+    */
+    public String setAttribute2(String attribute2) {
+        this.attribute2 = attribute2;
+    }
+
+				/**
     * @generated NOT
     */
     public int setAttribute2(int attribute2) {
